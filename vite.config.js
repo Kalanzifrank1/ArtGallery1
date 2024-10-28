5c7382fa7a43de5b969afba583234538b989d3d6
Kalanzifrank1 <kalanzifrank5@gmail.com>
2024-10-28 10:18:18 -0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+//import tailwindcss from 'tailwindcss'
+//import autoprefixer from 'autoprefixer'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-          tailwindcss(),
-          autoprefixer()
+  plugins: [react(),         
   ],
 })
